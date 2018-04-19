f52d5c0f33e554a6c37780d4901e39f5ceb19b62
Yarisson <gemini8811@gmail.com>
2018-04-19 23:36:06 +0300

    лечение для customValidity, добавление констант, изменение названий обработчиков событий...

--- js/pictures.js
+++ js/pictures.js
@@ -252,63 +252,66 @@ imgUploadEffects.addEventListener('click', uploadLevelPin);
 resizeControlPlus.addEventListener('click', resizePlus);
 resizeControlMinus.addEventListener('click', resizeMinus);
 
+var MAX_HASHTAG_LENGTH = 20;
+var MAX_NUMBER_OF_HASHTAGS = 5;
+var buttonSubmitElement = document.querySelector('.img-upload__submit');
+var inputHashTagsElement = document.querySelector('.text__hashtags');
+var textareaCommentsElement = document.querySelector('.text__description');
 
-var buttonSubmit = document.querySelector('.img-upload__submit');
-var inputHashTags = document.querySelector('.text__hashtags');
-var textareaComments = document.querySelector('.text__description');
+var setErrorMessageToHashTags = function (message) {
+  inputHashTagsElement.setCustomValidity(message);
+  inputHashTagsElement.style.border = '2px solid red';
+};
 
-var inputHashTagsFocus = function () {
+var onInputHashTagsFocus = function () {
   document.removeEventListener('keydown', uploadCancelClickEsc);
 };
 
-var inputHashTagsBlur = function () {
+var onInputHashTagsBlur = function () {
   document.addEventListener('keydown', uploadCancelClickEsc);
 };
 
-var textareaFocus = function () {
+var onTextareaFocus = function () {
   document.removeEventListener('keydown', uploadCancelClickEsc);
 };
 
-var textareaBlur = function () {
+var onTextareaBlur = function () {
   document.addEventListener('keydown', uploadCancelClickEsc);
 };
 
-var validationHashTags = function () {
+var validateHashTags = function () {
   var SEPARATOR = ' ';
-  var hashTagString = inputHashTags.value.toLowerCase();
+  var hashTagString = inputHashTagsElement.value.toLowerCase();
   var hashTagsArray = hashTagString.split(SEPARATOR);
-  if (hashTagsArray.length > 5) {
-    inputHashTags.setCustomValidity('Количество хэш-тегов не может быть больше 5');
+  if (hashTagsArray.length > MAX_NUMBER_OF_HASHTAGS) {
+    setErrorMessageToHashTags('Количество хэш-тегов не может быть больше 5');
     return;
   }
   for (var i = 0; i < hashTagsArray.length; i++) {
     if (hashTagsArray[i].charAt(0) !== '#') {
-      inputHashTags.setCustomValidity('Отсутствует символ # в начале хэш-тега');
-      inputHashTags.style.border = '2px solid red';
+      setErrorMessageToHashTags('Отсутствует символ # в начале хэш-тега');
       return;
     }
-    if (hashTagsArray[i].length > 20) {
-      inputHashTags.setCustomValidity('Длина одного хэш-тега не должна быть больше символов 20');
+    if (hashTagsArray[i].length > MAX_HASHTAG_LENGTH) {
+      setErrorMessageToHashTags('Длина одного хэш-тега не должна быть больше символов 20');
       return;
     }
     var hashTagIndex = i;
     for (var j = hashTagIndex + 1; j < hashTagsArray.length; j++) {
       if (hashTagsArray[i] === hashTagsArray[j]) {
-        inputHashTags.setCustomValidity('Нельзя использовать одинаковые хэш-теги');
-        inputHashTags.style.border = '2px solid red';
+        setErrorMessageToHashTags('Нельзя использовать одинаковые хэш-теги');
         return;
       }
     }
   }
 };
 
-var buttonFormSubmit = function (evt) {
-  evt.preventDefault();
-  validationHashTags();
+var buttonFormClickHandler = function () {
+  validateHashTags();
 };
 
-inputHashTags.addEventListener('focus', inputHashTagsFocus);
-buttonSubmit.addEventListener('click', buttonFormSubmit);
-inputHashTags.addEventListener('blur', inputHashTagsBlur);
-textareaComments.addEventListener('focus', textareaFocus);
-textareaComments.addEventListener('blur', textareaBlur);
+inputHashTagsElement.addEventListener('focus', onInputHashTagsFocus);
+buttonSubmitElement.addEventListener('click', buttonFormClickHandler);
+inputHashTagsElement.addEventListener('blur', onInputHashTagsBlur);
+textareaCommentsElement.addEventListener('focus', onTextareaFocus);
+textareaCommentsElement.addEventListener('blur', onTextareaBlur);
