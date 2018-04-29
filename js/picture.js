28bd9dd860b4b27946a41e7d9f8e9e501415f6ba
Yarisson <gemini8811@gmail.com>
2018-04-30 00:11:02 +0300

    правки в подключение функции загрузки данных с сервера

--- js/picture.js
+++ js/picture.js
@@ -8,38 +8,32 @@
     var pictureElement = pictureTemplate.cloneNode(true);
     pictureElement.querySelector('.picture__img').setAttribute('src', picture.url);
     pictureElement.querySelector('.picture__img').setAttribute('data-element-id', index);
-    pictureElement.querySelector('.picture__stat--comments').textContent = window.data.COMMENTS[0];
+    pictureElement.querySelector('.picture__stat--comments').textContent = picture.comments.length;
     pictureElement.querySelector('.picture__stat--likes').textContent = picture.likes;
     return pictureElement;
   };
 
-  /*  var paintingPictures = function (array) {
+  var paintingPictures = function (array) {
     var picturesDraw = document.querySelector('.pictures');
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < array.length - 1; i++) {
       fragment.appendChild(renderPicture(array[i], i));
     }
     picturesDraw.appendChild(fragment);
-  }; */
-
-  window.backend.load(function (pictures) {
-    var picturesDraw = document.querySelector('.pictures');
-    var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < pictures.length; i++) {
-      fragment.appendChild(renderPicture(pictures[i]));
-    }
-    picturesDraw.appendChild(fragment);
-    arrayPictures = pictures;
-  });
-
-  /*  var initPictures = function () {
-    var pictures = window.backend.load();
-    paintingPictures(pictures);
-    arrayPictures = pictures;
   };
 
-  initPictures(); */
+  var initPictures = function () {
+    window.backend.load(function (pictures) {
+      paintingPictures(pictures);
+      arrayPictures = pictures;
+      return arrayPictures;
+    },
+    function (onError) {
+    }
+    );
+  };
+
+  initPictures();
 
   window.picture = {
     arrayPictures: arrayPictures
