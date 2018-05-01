c0f6b3419c1d89888ac43bd0e6bfdb2d6609f556
Yarisson <gemini8811@gmail.com>
2018-05-02 00:16:10 +0300

    создание обработчиков событий для фильтрации загруженных изображений

--- js/picture.js
+++ js/picture.js
@@ -3,6 +3,63 @@
 (function () {
   var arrayPictures = [];
 
+  var imgFilters = document.querySelector('.img-filters');
+  var filterPopular = document.querySelector('#filter-popular');
+  var filterNew = document.querySelector('#filter-new');
+  var filterDiscussed = document.querySelector('#filter-discussed');
+  var pictureLinks = document.querySelectorAll('.picture__link');
+
+  var sortArrayLikes = function (array) {
+    array.sort(function (a, b) {
+      if (a.likes > b.likes) {
+        return -1;
+      }
+      if (a.likes < b.likes) {
+        return 1;
+      }
+      // a должно быть равным b
+      return 0;
+    });
+    paintingPictures(array);
+  };
+
+  var sortArrayComments = function (array) {
+    array.sort(function (a, b) {
+      if (a.comments.length > b.comments.length) {
+        return -1;
+      }
+      if (a.comments.length < b.comments.length) {
+        return 1;
+      }
+      // a должно быть равным b
+      return 0;
+    });
+    paintingPictures(array);
+  };
+
+  var onFilterPopularClick = function () {
+    pictureLinks.parentNode.removeChild(pictureLinks);
+    filterPopular.classList.add('img-filters__button--active');
+    filterNew.classList.remove('img-filters__button--active');
+    filterDiscussed.classList.remove('img-filters__button--active');
+    sortArrayLikes(window.picture.arrayPictures);
+  };
+
+  var onFilterNewClick = function () {
+    pictureLinks.parentNode.removeChild(pictureLinks);
+    filterNew.classList.add('img-filters__button--active');
+    filterPopular.classList.remove('img-filters__button--active');
+    filterDiscussed.classList.remove('img-filters__button--active');
+  };
+
+  var onFilterDiscussedClick = function () {
+    pictureLinks.parentNode.removeChild(pictureLinks);
+    filterDiscussed.classList.add('img-filters__button--active');
+    filterNew.classList.remove('img-filters__button--active');
+    filterPopular.classList.remove('img-filters__button--active');
+    sortArrayComments(window.picture.arrayPictures);
+  };
+
   var renderPicture = function (picture, index) {
     var pictureTemplate = document.querySelector('#picture').content;
     var pictureElement = pictureTemplate.cloneNode(true);
@@ -38,6 +95,7 @@
 
   var initPictures = function () {
     window.backend.load(function (pictures) {
+      imgFilters.classList.remove('img-filters--inactive');
       paintingPictures(pictures);
       window.picture.arrayPictures = pictures;
       window.gallery.addEventListenerOnPictures();
@@ -50,9 +108,14 @@
 
   initPictures();
 
+  filterPopular.addEventListener('click', onFilterPopularClick);
+  filterNew.addEventListener('click', onFilterNewClick);
+  filterDiscussed.addEventListener('click', onFilterDiscussedClick);
+
   window.picture = {
     arrayPictures: arrayPictures,
-    elementErrorShow: elementErrorShow
+    elementErrorShow: elementErrorShow,
+    pictureLinks: pictureLinks
   };
 
 })();
